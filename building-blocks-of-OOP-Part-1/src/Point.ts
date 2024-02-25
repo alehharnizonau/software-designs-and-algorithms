a79c385524e67fa014f5156e3dc0b980650cf13f
Aleh Harnizonau <aleh.harnizonau@sap.com>
2024-02-25 14:49:37 +0300

    refactor distance method

--- building-blocks-of-OOP-Part-1/src/Point.ts
+++ building-blocks-of-OOP-Part-1/src/Point.ts
@@ -1,3 +1,10 @@
+interface Points {
+    x1: number;
+    x2: number;
+    y1: number;
+    y2: number;
+}
+
 export class Point {
     x: number;
     y: number;
@@ -14,19 +21,15 @@ export class Point {
     distance(): number;
     distance(x: Point): number;
     distance(x?: number | Point, y?: number): number {
-        if (!arguments.length) {
-            return this.calculate(this.x, this.y);
-        }
-
         if (x instanceof Point) {
             const [x1, y1] = x.toString().replace(/[\(\)\s]/g, "").split(',');
-            return this.calculate(Number(x1), Number(y1), this.x, this.y);
+            return this.calculate({x2: Number(x1), y2: Number(y1), x1: this.x, y1: this.y});
         }
 
-        return this.calculate(this.x, this.y, x, y);
+        return this.calculate({x2: this.x, y2: this.y, x1: x || 0, y1: y || 0});
     }
 
-    calculate(x2, y2, x1 = 0, y1 = 0) {
+    calculate({x1, x2, y1, y2}: Points): number {
         return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
 }
