f09c737974e3f52b2a98cedc69b4f420749d0b5a
Aleh Harnizonau <aleh.harnizonau@sap.com>
2024-02-25 16:14:53 +0300

    add shape class solution

--- building-blocks-of-OOP-Part-1/src/Shape.ts
+++ building-blocks-of-OOP-Part-1/src/Shape.ts
@@ -1,3 +1,30 @@
+import {Point} from "./Point";
+
 export abstract class Shape {
-  abstract getType(): string;
+    points: Point[];
+    protected color: string;
+    protected filled: boolean;
+
+    constructor(points: Point[]);
+    constructor(points: Point[], color?: string, filled?: boolean) {
+        if (points.length < 3) {
+            throw new Error()
+        }
+
+        this.points = points;
+        this.color = color || 'green';
+        this.filled = filled === undefined ? true : filled;
+    }
+
+    toString() {
+        return `A Shape with color of ${this.color} and ${this.filled ? 'filled' : 'not filled'}. Points: ${this.points.join(', ')}.`
+    }
+
+    getPerimeter() {
+        return this.points.reduce((acc, cum, index) => this.points.length === index
+            ? acc + cum.distance(this.points[0])
+            : acc + cum.distance(this.points[index + 1]), 0);
+    }
+
+    abstract getType(): string;
 }
